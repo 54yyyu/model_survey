3e655032a25a901f4d9b3ab56c07e82c68d826cd
Steven Yu <yiyu@KooLab-CSHL>
2022-08-05 11:22:40 -0400

    pearson

--- model_survey/utils.py
+++ model_survey/utils.py
@@ -9,6 +9,10 @@ def spearman_r(y_true, y_pred):
     return (tf.py_function(spearmanr, [tf.cast(y_pred, tf.float32), 
             tf.cast(y_true, tf.float32)], Tout = tf.float32) )
 
+def pearson_r(y_true, y_pred):
+    return (tf.py_function(pearsonr, [tf.cast(y_pred, tf.float32), 
+            tf.cast(y_true, tf.float32)], Tou = tf.float32) )
+
 def create_deepstar(input_shape):
     inputs, outputs = deepstar.create(input_shape)
     return keras.Model(inputs=inputs, outputs=outputs)
